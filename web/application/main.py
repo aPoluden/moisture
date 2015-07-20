19bb5757393161b85031559c35698d5a845bb457
Artiom <artiomnkkm@gmail.com>
2015-07-20 19:43:35 +0300

    Pump on/off/status

--- web/application/main.py
+++ web/application/main.py
@@ -1,5 +1,6 @@
 import os, time, datetime, db
 from application import app
+from model import pump
 
 @app.route('/data/<int:value>',  methods = ['POST', 'GET'])
 def receiveData(value):
@@ -10,3 +11,25 @@ def receiveData(value):
 def getTimeStamp():
 	ts = time.time()
 	return datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+
+@app.route('/pump/on',  methods = ['PUT', 'GET'])
+def turnOn():
+	if pump.working is False:
+		# Pump turn on logic
+	    pump.working = True
+	    return "Pump turned on"
+	else:
+		return "Check pump status"
+
+@app.route('/pump/off',  methods = ['PUT', 'GET'])
+def turnOff():
+	if pump.working is True:
+		# Pump turn off logic
+		pump.working = False
+		return "Pump turned off"
+	else:
+		return "Check pump status"
+
+@app.route('/pump/status', methods = ['GET'])
+def status():
+	return pump.status()
