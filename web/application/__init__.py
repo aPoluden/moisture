ba76aa47b30ab41a8074154bd48a007ddb583023
Artiom <artiomnkkm@gmail.com>
2015-08-18 19:55:25 +0300

    Fix js

--- web/application/__init__.py
+++ web/application/__init__.py
@@ -2,8 +2,5 @@ from flask import Flask
 
 app = Flask(__name__)
 
-if __name__ == '__main__':
-    app.run()
-
-from application import main
 from application import views
+from application import main
--- web/run.py
+++ web/run.py
@@ -1,6 +1,7 @@
 #!flask/bin/python
 from application import app
 
+app.debug = True
 app.run(
 		port = 5000,
 		host = '0.0.0.0'
