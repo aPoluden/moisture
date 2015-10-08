130037137550ff8bc96e890983dbcbc0e314121a
Artiom <artiomnkkm@gmail.com>
2015-10-08 10:03:32 +0300

    Use ui-router

--- web/application/static/js/app.js
+++ web/application/static/js/app.js
@@ -1,3 +1,15 @@
-var app = angular.module('moisture', ['moisture.ctrls'])
+var app = angular.module('moisture', ['moisture.ctrls', 'ui.router'])
 
 	.constant("socket", "http://127.0.0.1:5000")
+
+   .config(function($stateProvider, $urlRouterProvider) {
+
+   		$urlRouterProvider.otherwise("/dashboard");
+
+    	$stateProvider
+	    	.state('dashboard', {
+	      		url: '/dashboard',
+	      		templateUrl: 'static/templates/dashboard.html',
+	      		controller: 'PumpCtrl'
+	    })
+   });
